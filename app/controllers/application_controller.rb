162427d80bb171f20ad96ee323656ecaf44d01e6
Pedhaf <pedro22ahferreira@gmail.com>
2023-04-06 17:51:16 -0300

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+    def hello
+        render plai: "hellow world"
+    end
+
+    def about
+
+    end
+
+    def help 
+        
+    end
+    
+end
